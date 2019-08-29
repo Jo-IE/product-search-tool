b9ded025e78cd4a96d8f83be5bfaf074bbae4c9d
Jo IE <joie.software@gmail.com>
2019-08-29 18:30:22 +0100

    edit landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import Hero from "./Hero";
+import ProductList from "./ProductList";
+import { ProductConsumer } from "../context";
 
 class LandingPage extends Component {
   state = {};
   render() {
-    return <div>landing</div>;
+    return (
+      <ProductConsumer>
+        {value => {
+          return (
+            <React.Fragment>
+              <Hero />
+              <ProductList />
+            </React.Fragment>
+          );
+        }}
+      </ProductConsumer>
+    );
   }
 }
 
