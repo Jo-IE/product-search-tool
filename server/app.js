8781548e18394f7c9d551d0224c42194c48cb727
Jo IE <joie.software@gmail.com>
2019-09-04 00:06:34 +0100

    config files

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 const router = require("./routes/search");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", router);
