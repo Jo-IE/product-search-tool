2c71aab9ae4336bd299bd7ff2c7de22c301fd73d
Jo IE <joie.software@gmail.com>
2019-09-03 15:23:49 +0100

    replace console.logs with debug

--- server/routes/search.js
+++ server/routes/search.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body } = require("express-validator");
 const { validationResult } = require("express-validator");
 const fetch = require("node-fetch");
+const debug = require("debug")("q-hunt");
 
 let productname;
 
@@ -20,7 +21,7 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // There are errors, remain on landing page
-      //res.send(errors);
+
       res.redirect("/");
     } else {
       //no errors, change value of productname to value user entered
@@ -31,19 +32,9 @@ router.post(
 );
 
 router.get("/display-products", (req, res) => {
-  const API_Key =
-    "007bd065572b0831c9ba2b7d72b546613329d0cd1a459c9bcaf8cf93d503b3a6";
-
-  const API_Secret =
-    "6b9c2402775dfb7ee117a555b69828e343e5973695b23a4e4b8c46b6105c17f5";
-  const TOKEN =
-    "0c9e7ed62aeb763ee64334891a454ac0ba5c159f7fcfd97c4cf890236b62ecc5";
-  const ACCESS_TOKEN =
-    "abc16faa925a28c5ea452ed132f22b934c94fa21ccc633a1ce2a9881422778c4";
-  const query = `query{ posts { edges { node { id, name } } } }`;
   const opts = {
     headers: {
-      Authorization: `Bearer ${ACCESS_TOKEN}`,
+      Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
       Accept: "application/json",
       "Content-Type": "application/json"
     },
@@ -64,7 +55,7 @@ router.get("/display-products", (req, res) => {
                 type
                 url
               }
-              website
+              
               reviewsRating
 }}}}`
     })
@@ -79,7 +70,7 @@ router.get("/display-products", (req, res) => {
       res.send(json.data.posts.edges);
     })
     .catch(err => {
-      console.log(err);
+      debug(err);
     });
 });
 
